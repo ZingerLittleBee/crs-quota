e760e707a36cb6f4760e8694684bc70411954fb1
ZingerLittleBee <6970999@gmail.com>
2026-01-03 11:52:46 +0800

    Initial Commit

---
+++ crs-quotio/crs_quotioApp.swift
@@ -0,0 +1,17 @@
+//
+//  crs_quotioApp.swift
+//  crs-quotio
+//
+//  Created by ZingerBee on 2026/1/3.
+//
+
+import SwiftUI
+
+@main
+struct crs_quotioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
